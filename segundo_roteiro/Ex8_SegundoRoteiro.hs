df081cc9f81cbce2e4ef5e24139302a931d00302
MaestriIza <izabelapmaestri@gmail.com>
2021-03-29 02:00:47 -0300

    ex de 4 a 8

---
+++ segundo_roteiro/Ex8_SegundoRoteiro.hs
@@ -0,0 +1,7 @@
+type Data = (Int, Int, Int)
+
+type Livro = (String, String, String, String, Int)
+
+type Aluno = (String, String, String, String)
+
+type Emprestimo = (String, String, Data, Data, String)
