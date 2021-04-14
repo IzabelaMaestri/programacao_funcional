c9739dd95b96d1742cfc31a54de17f26519eeee2
MaestriIza <izabelapmaestri@gmail.com>
2021-04-14 00:32:35 -0300

    ex5

--- primeiro_trabalho/Ex5.hs
+++ primeiro_trabalho/Ex5.hs
@@ -1,2 +1,5 @@
 contaNegMg2:: [Int]->Int
-contaNegMg2 xs = [length(x)| x<-xs, x>=0 && x mod 3 == 0 ]
+contaNegMg2 xs = [length(x)| x<-xs, x>0 && x mod 3 = 0 ]
+
+listaNegM2 :: [Int] -> [Int]
+listaNegM2 xs = [x|x<-xs, x>0 && x mod 3 = 0 ]
