885d672dcd8b0161dcd4cea0f1780350d0167be2
MaestriIza <izabelapmaestri@gmail.com>
2021-04-13 23:39:24 -0300

    ex1

--- primeiro_trabalho/Ex1.hs
+++ primeiro_trabalho/Ex1.hs
@@ -1,6 +1,6 @@
 analisa_raizes:: Int -> Int -> Int -> Int
 analisa_raizes a b c 
-    | (b^2) > (4*a*c) = 1
-    | (b^2) == (4*a*c) = 2
-    | (b^2) < (4*a*c) = 3 
+    | (b*b) > (4*a*c) = 1
+    | (b*b) == (4*a*c) = 2
+    | (b*b) < (4*a*c) = 3 
     |otherwise = 4
