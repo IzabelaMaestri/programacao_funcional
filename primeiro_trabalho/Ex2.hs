99c37283712f3ac7649071dd5acb69e1840f39de
MaestriIza <izabelapmaestri@gmail.com>
2021-04-13 23:42:47 -0300

    ex2

--- primeiro_trabalho/Ex2.hs
+++ primeiro_trabalho/Ex2.hs
@@ -1,11 +1,11 @@
-segundo_grau:: Double -> Double -> Double -> (Double, Double)
-segundo_grau a b c = ((-b - (sqrt (b*b - 4*a*c)))/(2*a), (-b + (sqrt (b*b - 4*a*c)))/(2*a))
+type primeiro = (Double)
+type segundo = (Double, Double)
 
-primeiro_grau:: Double -> Double -> Double
-primeiro_grau b c = ((-c)/b)
+segundo:: Double -> Double -> Double -> (Double, Double)
+segundo a b c = ((-b - (sqrt (b*b - 4*a*c)))/(2*a), (-b + (sqrt (b*b - 4*a*c)))/(2*a))
+
+primeiro:: Double -> Double -> Double
+primeiro b c = ((-c)/b)
 
 equacao:: Double -> Double -> Double -> (Double, Double)
-equacao a b c = if (a == 0) then (a, primeiro_grau) else (segundo_grau)
-
-
-
+equacao a b c = if (a == 0) then (a, primeiro) else (segundo)
